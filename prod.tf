33337ba8bb8e63ebbb0d21b954977f07436f75a3
Bahaa Noah <bahaa.noah93@gmail.com>
2022-09-21 22:00:55 +0400

    added vars

--- prod.tf
+++ prod.tf
@@ -1,3 +1,22 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_min_size" {
+  type = number
+}
+variable "web_max_size" {
+  type = number
+}
+variable "web_desired_capacity" {
+  type = number
+}
+
 provider "aws" {
   profile = "terraform"
   region  = "me-central-1"
@@ -31,14 +50,14 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
 
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
 
 
@@ -46,7 +65,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
 
   tags = {
@@ -54,32 +73,32 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count                  = 2
-  ami                    = "ami-03ccfaecad4b0f79e"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.prod_web.id]
+# resource "aws_instance" "prod_web" {
+#   count                  = 2
+#   ami                    = "ami-03ccfaecad4b0f79e"
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [aws_security_group.prod_web.id]
 
-  tags = {
-    "Terraform" = "true"
-  }
-}
+#   tags = {
+#     "Terraform" = "true"
+#   }
+# }
 
 
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" = "true"
-  }
-}
+# resource "aws_eip_association" "prod_web" {
+#   instance_id   = aws_instance.prod_web.0.id
+#   allocation_id = aws_eip.prod_web.id
+# # }
+# resource "aws_eip" "prod_web" {
+#   tags = {
+#     "Terraform" = "true"
+#   }
+# }
 
 
 resource "aws_elb" "prod_web" {
-  name            = "prod-web"
-  instances       = aws_instance.prod_web.*.id
+  name = "prod-web"
+  #   instances       = aws_instance.prod_web.*.id
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
   listener {
@@ -91,4 +110,41 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" = "true"
   }
+}
+
+
+resource "aws_launch_configuration" "prod_web" {
+  name_prefix   = "prod-wb"
+  image_id      = var.web_image_id
+  instance_type = var.web_instance_type
+
+  security_groups = [aws_security_group.prod_web.id]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  min_size             = var.web_min_size
+  max_size             = var.web_max_size
+  desired_capacity     = var.web_desired_capacity
+  launch_configuration = aws_launch_configuration.prod_web.id
+  vpc_zone_identifier  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb_target_group" "prod_web" {
+  name     = "prod-web"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  alb_target_group_arn   = aws_lb_target_group.prod_web.arn
 }
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+whitelist            = ["0.0.0.0/0"]
+web_image_id         = "ami-03ccfaecad4b0f79e"
+web_instance_type    = "t3.micro"
+web_min_size         = 1
+web_max_size         = 2
+web_desired_capacity = 1
+
+
